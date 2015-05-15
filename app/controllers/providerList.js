dd4432acf4d578f1f4149b94815fb51b490d281d
Ben Woodruff <benjamin.woodruff@udri.udayton.edu>
2015-05-15 10:43:57 -0400

    corrected spelling error

--- app/controllers/providerList.js
+++ app/controllers/providerList.js
@@ -4,7 +4,7 @@ function info(){
 	
 var alertDialog = Titanium.UI.createAlertDialog({
    // title: 'Organization Name',
-    message: 'Connect with agencies to learn what programs are available for famalies and children in the comunity',
+    message: 'Connect with agencies to learn what programs are available for families and children in the comunity',
     buttonNames: ['Close', 'Connect'],
     cancel: 1
 });
